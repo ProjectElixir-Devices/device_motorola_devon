96ede3a5f3d58e0e5ece0d85c2c4355492c5f781
FPSensor <gkartyt@gmail.com>
2023-08-16 11:45:15 +0000

    devon: elixir-ify

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-elixir
 
 # Permissions
 PRODUCT_COPY_FILES += \
